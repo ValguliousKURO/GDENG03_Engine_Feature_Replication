cce2346543725a537056edb10a8d4e9a593f8f28
RonaldMcpogger <rameses_amar@dlsu.edu.ph>
2026-08-08 22:09:16 +0800

    debugmanager

--- DirectXGameEngine/DX3D/Source/DX3D/Core/Logger.cpp
+++ DirectXGameEngine/DX3D/Source/DX3D/Core/Logger.cpp
@@ -2,10 +2,12 @@
 
 //events 
 
-#include <DX3D/EventBroadcasting/EventBroadcastManager.h>
-#include <DX3D/EventBroadcasting/EventNames.h>
-#include <DX3D/EventBroadcasting/Parameters.h>
+//#include <DX3D/EventBroadcasting/EventBroadcastManager.h>
+//#include <DX3D/EventBroadcasting/EventNames.h>
+//#include <DX3D/EventBroadcasting/Parameters.h>
 #include <iostream>
+
+#include <DX3D/Core/DebugLogManager.h>
 
 dx3d::Logger::Logger(LogLevel logLevel): m_logLevel(logLevel)
 {
@@ -33,8 +35,6 @@ void dx3d::Logger::_log(LogLevel level, const char* message)
 	std::clog << "{Dx3D " << logLevelToString(level) << "]: " << message << "\n";
 	
 	//event broadcast the string
-	auto params = dx3d::Parameters{};
-	params.PutExtra("Message", std::string(message));
-	
-	dx3d::EventBroadcastManager::getInstance().postEvent(dx3d::EventNames::ON_DEBUG_LOG_ENTRY, params);
+	DebugLogManager::getInstance().customLog(message);
+
 }
